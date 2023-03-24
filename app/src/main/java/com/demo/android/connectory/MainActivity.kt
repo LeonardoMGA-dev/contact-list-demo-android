42de546a867213fdb96237febd471936f747c7ec
Leonardo Glez <leonarmga@gmail.com>
2023-03-24 09:34:04 -0700

    EmployeeCard List

--- app/src/main/java/com/demo/android/connectory/MainActivity.kt
+++ app/src/main/java/com/demo/android/connectory/MainActivity.kt
@@ -4,40 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.demo.android.connectory.component.EmployeeCard
 import com.demo.android.connectory.ui.theme.ConnectoryTheme
+import com.demo.android.connectory.util.employeeFakeList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ConnectoryTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LazyColumn {
+                        items(employeeFakeList) {
+                            EmployeeCard(employee = it)
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ConnectoryTheme {
-        Greeting("Android")
     }
 }
