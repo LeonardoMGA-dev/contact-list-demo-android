7c37a1838ed41a05a1cd196017a765b681b17ed6
Leonardo Glez <leonarmga@gmail.com>
2023-03-24 14:29:40 -0700

    EmployeeCard expanding animation

--- app/src/main/java/com/demo/android/connectory/presentation/component/EmployeeCard.kt
+++ app/src/main/java/com/demo/android/connectory/presentation/component/EmployeeCard.kt
@@ -1,52 +1,69 @@
 package com.demo.android.connectory.presentation.component
 
+import android.telephony.PhoneNumberUtils
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import com.demo.android.connectory.R
 import com.demo.android.connectory.domain.entity.Employee
 import com.valentinilk.shimmer.shimmer
+import java.util.*
 
 @Composable
 fun EmployeeCard(employee: Employee) {
-    // TODO: implement expanding animation
-    // TODO: Add chevron
+    val formattedNumber =
+        PhoneNumberUtils.formatNumber(employee.phoneNumber, Locale.getDefault().country)
+    var isExpanded by remember { mutableStateOf(false) }
+    val rotateState = animateFloatAsState(
+        targetValue = if (isExpanded) 180F else 0F,
+    )
     Card(
         shape = RoundedCornerShape(16.dp),
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp, vertical = 8.dp)
     ) {
-        var isExpanded by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
                 .clickable { isExpanded = !isExpanded }
                 .padding(16.dp)
         ) {
             Row(verticalAlignment = Alignment.CenterVertically) {
-                AsyncImage(
+                SubcomposeAsyncImage(
                     model = ImageRequest.Builder(LocalContext.current)
                         .data(employee.photoUrlSmall)
                         .error(R.drawable.ic_launcher_background)
                         .crossfade(true)
                         .build(),
-                    placeholder = painterResource(R.drawable.ic_launcher_background),
+                    loading = {
+                        Box(
+                            modifier = Modifier
+                                .clip(CircleShape)
+                                .shimmer()
+                                .size(72.dp)
+                        )
+                    },
                     contentDescription = "Employee photo",
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
@@ -62,16 +79,23 @@ fun EmployeeCard(employee: Employee) {
                     )
                     Text(text = employee.team, fontSize = 14.sp)
                 }
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    Icons.Default.ArrowDropDown, "",
+                    modifier = Modifier.rotate(rotateState.value)
+                )
             }
-            if (isExpanded) {
-                Spacer(modifier = Modifier.height(16.dp))
-                Divider()
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(text = "Phone: ${employee.phoneNumber}", fontSize = 14.sp)
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = "Email: ${employee.emailAddress}", fontSize = 14.sp)
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = "Bio: ${employee.biography}", fontSize = 14.sp)
+            AnimatedVisibility(visible = isExpanded) {
+                Column {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Divider()
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(text = "Phone: $formattedNumber", fontSize = 14.sp)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "Email: ${employee.emailAddress}", fontSize = 14.sp)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(text = employee.biography, fontSize = 14.sp)
+                }
             }
         }
     }
