fbb7b7f84f52f8a72e85ac21ff7b3a481e5eed2f
Leonardo Glez <leonarmga@gmail.com>
2023-03-24 09:32:50 -0700

    Employee fake data

---
+++ app/src/main/java/com/demo/android/connectory/util/FakeData.kt
@@ -0,0 +1,125 @@
+package com.demo.android.connectory.util
+
+import com.demo.android.connectory.domain.Employee
+
+
+val employeeFakeList = listOf(
+    Employee(
+        "some-uuid-1",
+        "Eric Rogers",
+        "5556669870",
+        "erogers.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path1.jpg",
+        "https://some.url/path2.jpg",
+        "Seller",
+        "FULL_TIME"
+    ),
+    Employee(
+        "some-uuid-2",
+        "Jane Doe",
+        "5551234567",
+        "jdoe.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path3.jpg",
+        "https://some.url/path4.jpg",
+        "Developer",
+        "PART_TIME"
+    ),
+    Employee(
+        "some-uuid-3",
+        "John Smith",
+        "5555555555",
+        "jsmith.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path5.jpg",
+        "https://some.url/path6.jpg",
+        "Designer",
+        "CONTRACTOR"
+    ),
+    Employee(
+        "some-uuid-4",
+        "Sarah Johnson",
+        "5557778888",
+        "sjohnson.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path7.jpg",
+        "https://some.url/path8.jpg",
+        "Seller",
+        "FULL_TIME"
+    ),
+    Employee(
+        "some-uuid-5",
+        "Robert Lee",
+        "5554443333",
+        "rlee.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path9.jpg",
+        "https://some.url/path10.jpg",
+        "Developer",
+        "PART_TIME"
+    ),
+    Employee(
+        "some-uuid-6",
+        "Maggie Wong",
+        "5552221111",
+        "mwong.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path11.jpg",
+        "https://some.url/path12.jpg",
+        "Marketing",
+        "FULL_TIME"
+    ),
+    Employee(
+        "some-uuid-7",
+        "David Kim",
+        "5558889999",
+        "dkim.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path13.jpg",
+        "https://some.url/path14.jpg",
+        "Designer",
+        "CONTRACTOR"
+    ),Employee(
+        "some-uuid-7",
+        "David Kim",
+        "5558889999",
+        "dkim.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path13.jpg",
+        "https://some.url/path14.jpg",
+        "Designer",
+        "CONTRACTOR"
+    ),Employee(
+        "some-uuid-7",
+        "David Kim",
+        "5558889999",
+        "dkim.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path13.jpg",
+        "https://some.url/path14.jpg",
+        "Designer",
+        "CONTRACTOR"
+    ),Employee(
+        "some-uuid-7",
+        "David Kim",
+        "5558889999",
+        "dkim.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path13.jpg",
+        "https://some.url/path14.jpg",
+        "Designer",
+        "CONTRACTOR"
+    ),
+    Employee(
+        "some-uuid-7",
+        "David Kim",
+        "5558889999",
+        "dkim.demo@squareup.com",
+        "A short biography describing the employee.",
+        "https://some.url/path13.jpg",
+        "https://some.url/path14.jpg",
+        "Designer",
+        "CONTRACTOR"
+    ),
+)
