a6a5c853ec9b328ccc93f99e8c38b59789728e44
Leonardo Glez <leonarmga@gmail.com>
2023-03-24 11:00:24 -0700

    Get Employees Endpoint service refactor

--- app/src/main/java/com/demo/android/connectory/data/networking/ConnectoryService.kt
+++ app/src/main/java/com/demo/android/connectory/data/networking/ConnectoryService.kt
@@ -2,7 +2,9 @@ package com.demo.android.connectory.data.networking
 
 import com.demo.android.connectory.data.networking.dto.GetEmployeesResponseDto
 import retrofit2.Response
+import retrofit2.http.GET
 
 interface ConnectoryService {
+    @GET("employees.json")
     suspend fun getEmployees(): Response<GetEmployeesResponseDto>
 }
