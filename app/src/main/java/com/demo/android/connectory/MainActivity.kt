169e5a2227fc7ed58553966b0cb196d6c5bfb983
Leonardo Glez <leonarmga@gmail.com>
2023-03-24 10:05:00 -0700

    Image loading with Coil

--- app/src/main/java/com/demo/android/connectory/MainActivity.kt
+++ app/src/main/java/com/demo/android/connectory/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     LazyColumn {
                         items(employeeFakeList) {
+                            // TODO: implement appearing animation
                             EmployeeCard(employee = it)
                         }
                     }
--- app/src/main/java/com/demo/android/connectory/component/EmployeeCard.kt
+++ app/src/main/java/com/demo/android/connectory/component/EmployeeCard.kt
@@ -1,6 +1,5 @@
 package com.demo.android.connectory.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -11,15 +10,21 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.demo.android.connectory.R
 import com.demo.android.connectory.domain.Employee
 
 @Composable
 fun EmployeeCard(employee: Employee) {
+    // TODO: implement expanding animation
+    // TODO: Add chevron
     Card(
         shape = RoundedCornerShape(16.dp),
         modifier = Modifier
@@ -33,12 +38,18 @@ fun EmployeeCard(employee: Employee) {
                 .padding(16.dp)
         ) {
             Row(verticalAlignment = Alignment.CenterVertically) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(employee.photoUrlSmall)
+                        .error(R.drawable.ic_launcher_background)
+                        .crossfade(true)
+                        .build(),
+                    placeholder = painterResource(R.drawable.ic_launcher_background),
                     contentDescription = "Employee photo",
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .size(72.dp)
                         .clip(CircleShape)
+                        .size(72.dp)
                 )
                 Spacer(modifier = Modifier.width(16.dp))
                 Column {
