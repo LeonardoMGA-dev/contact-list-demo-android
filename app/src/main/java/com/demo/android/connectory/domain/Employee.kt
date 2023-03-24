b7cd5e2656e9e9c431e994eb4beecb77c39c63dd
Leonardo Glez <leonarmga@gmail.com>
2023-03-24 09:32:00 -0700

    Employee domain entitiy

---
+++ app/src/main/java/com/demo/android/connectory/domain/Employee.kt
@@ -0,0 +1,13 @@
+package com.demo.android.connectory.domain
+
+data class Employee(
+    val uuid: String,
+    val fullName: String,
+    val phoneNumber: String,
+    val emailAddress: String,
+    val biography: String,
+    val photoUrlSmall: String,
+    val photoUrlLarge: String,
+    val team: String,
+    val employeeType: String
+)
